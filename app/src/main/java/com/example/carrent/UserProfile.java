d6d9fba2ca86055cf192ff0c97bbb9eb49f9fcb1
satyamsharma17 <satyamsharma1725@gmail.com>
2022-04-13 01:14:01 +0530

    User Profile Activity added

---
+++ app/src/main/java/com/example/carrent/UserProfile.java
@@ -0,0 +1,18 @@
+package com.example.carrent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+    }
+}
